15cb9f7fe6a39c090e4669e370697382d252ab73
vamsi113 <krishultimate5@gmail.com>
2023-06-16 17:05:57 -0400

    change

--- alb.tf
+++ alb.tf
@@ -22,6 +22,13 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.internal? [var.vpc_cidr] : ["0.0.0.0/0"]
   }
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = var.internal? [var.vpc_cidr] : ["0.0.0.0/0"]
+  }
 
 
 
